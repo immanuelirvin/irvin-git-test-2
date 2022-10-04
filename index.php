a63e43ca7577a6fcb861e6c13b4cbe35509a11bd
immanuelirvin <immanuel.irvin@gmail.com>
2022-10-04 16:25:43 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h2>Hello World</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero aliquid hic fugit omnis temporibus doloribus earum, laborum vel minima accusantium et sit unde sint quidem modi voluptate, perspiciatis eveniet velit?</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
